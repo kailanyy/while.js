46ec0d77993e8bc43722bff942326ddfb647efba
kailanyy <kailanyiaramenco@gmail.com>
2021-11-24 14:59:44 -0300

    Init

---
+++ while.js/dowhile.js
@@ -0,0 +1,6 @@
+let numero = 3
+
+do{
+    numero = Math.floor(Math.random() * 10)
+    console.log("número randômico", numero);
+} while(numero != 3)
---
+++ while.js/ex2.js
@@ -0,0 +1,7 @@
+let n = 100
+
+for(let i = 0; i < n; i+=2){
+  if (i%2 == 0){
+    console.log(i,"Par")
+  }
+}
